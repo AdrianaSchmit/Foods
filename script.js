5eb31c769fe056e4ba4c362b2a50c62cc833b07d
AdranaSchmit <schmit.adriana@gmail.com>
2023-02-05 21:44:32 +0100

    Project fixed to be responsive

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.querySelector(".menu").addEventListener("click", () => {
     document.querySelectorAll(".target").forEach((item) => {
         item.classList.toggle("change")
-
     })
-} )
-
+})
 
 const icons = document.querySelectorAll(".section-1-icons i")
 let i = 1
@@ -16,14 +14,11 @@ setInterval(() => {
     const icon = document.querySelector(".section-1-icons .change")
 
     icon.classList.remove("change")
-    
 
-    if(i > icons.length) {
+    if (i > icons.length) {
         icons[0].classList.add("change")
-        i=1
+        i = 1
     } else {
         icon.nextElementSibling.classList.add("change")
     }
-
-
 }, 4000)
